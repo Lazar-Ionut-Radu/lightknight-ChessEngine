2a2a9b7c38f6ecd786f471c0ac158be670f3d9c0
Lazar-Ionut-Radu <lazarionutradu16@gmail.com>
2026-01-30 19:12:05 +0200

    Closes Issue #4

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,17 @@
 #include <board.h>
+#include <types.h>
+#include <movegen.h>
+#include <vector>
+#include <iostream>
 
 int main () 
 {
+    lightknight::Board pos = lightknight::Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    std::vector<lightknight::Move> moves;
+    
+    std::cout << lightknight::movegen::GenerateMoves(pos, moves) << std::endl;
+    for (lightknight::Move move : moves) {
+        std::cout << move << std::endl;
+    }
     return 0;
 }
